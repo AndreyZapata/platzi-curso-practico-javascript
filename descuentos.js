5d285c19b17913a3f9b2d222327dca4965549555
Andrey_Zapata <andrey.zapata@ciaempaques.com.co>
2022-06-16 11:07:33 -0500

    reto de cupon de descutno finalizado del taller #2 finalizado

--- descuentos.js
+++ descuentos.js
@@ -8,14 +8,44 @@ function calcularPrecioConDescuento(precio, descuento){
 }
 
 
+const coupons = [
+    "Junio20",
+    "primaRegalo",
+    "verano22",
+];
+
 function onClickButtonPriceDiscount(){
     const inputPrice = document.getElementById("InputPrice");
     const priceValue = inputPrice.value;
 
-    const inputDiscount = document.getElementById("InputDiscount");
-    const discountValue = inputDiscount.value;
+    // const inputDiscount = document.getElementById("InputDiscount");
+    // const discountValue = inputDiscount.value;
 
-    const precioConDescuento = calcularPrecioConDescuento(priceValue, discountValue);
+    const InputCoupon = document.getElementById("InputCoupon");
+    const couponValue = InputCoupon.value;
+
+    let discount;
+
+    switch(couponValue){
+
+        case coupons[0]:
+            descuento = 55;
+            break;
+
+        case coupons[1]:
+            descuento = 30;
+            break;
+
+        case coupons[2]:
+            descuento = 22;
+            break;
+
+        default:
+            descuento = 0;
+            break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
 
     const resultP = document.getElementById("Resultp");
 
@@ -31,3 +61,4 @@ function onClickButtonPriceDiscount(){
 //     porcentajePrecioConDescuento,
 //     precioConDescuento,
 // });
+
